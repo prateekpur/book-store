8ec04d33470feac25d33eeff04d21a848c29deae
Prateek Puri <prateekpuri@Prateeks-MacBook-Pro.local>
2024-11-28 07:54:04 -0500

    Updated dashboard

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { UserGroupIcon, HomeIcon, DocumentDuplicateIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
 const links = [
   { name: "Home", href: "/dashboard", icon: HomeIcon },
   { name: "Book", href: "/dashboard/books", icon: DocumentDuplicateIcon },
 ];
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import clsx from "clsx";
 
 export default function NavLinks() {
   const pathname = usePathname();

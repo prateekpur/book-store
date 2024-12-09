76fe3f0e514b92e0602e28eaaaefb2e8d74117b1
Prateek Puri <prateekpuri@yahoo.com>
2024-12-09 07:07:07 -0500

    Removed unused files

--- app/types.ts
+++
@@ -1,9 +0,0 @@
-export type Book = {
-  id: string;
-  name: string;
-  description: string;
-};
-
-export type BookTableType = {
-  books: Book[];
-};
--- jest.setup.ts
+++
@@ -1,1 +0,0 @@
-import '@testing-library/jest-dom'; // Extends jest with custom matchers
